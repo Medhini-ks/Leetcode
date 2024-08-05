53fad9a6faa6d1159487a775fe157848fad33985
Medhini-ks <119508392+Medhini-ks@users.noreply.github.com>
2024-08-05 10:51:18 +0530

    Time: 1 ms (86.19%), Space: 41.6 MB (57.15%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,26 +1,42 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String str="";
-        int i=0;
-        int j=0;
-        while(i<word1.length() && j<word2.length())
-        {
-            str+=word1.charAt(i);
-            str+=word2.charAt(j);
-            i++;
-            j++;
-        }
-        while(i<word1.length())
-        {
-            str+=word1.charAt(i);
-            i++;
-        }
+        // String str="";
+        // int i=0;
+        // int j=0;
+        // while(i<word1.length() && j<word2.length())
+        // {
+        //     str+=word1.charAt(i);
+        //     str+=word2.charAt(j);
+        //     i++;
+        //     j++;
+        // }
+        // while(i<word1.length())
+        // {
+        //     str+=word1.charAt(i);
+        //     i++;
+        // }
 
-        while(j<word2.length())
+        // while(j<word2.length())
+        // {
+        //     str+=word2.charAt(j);
+        //     j++;
+        // }
+        // return str;
+        StringBuilder str=new StringBuilder();
+        int i=0;
+        while(i<word1.length() || i< word2.length())
         {
-            str+=word2.charAt(j);
-            j++;
+            if(i<word1.length())
+            {
+                str.append(word1.charAt(i));
+            }
+
+            if(i<word2.length())
+            {
+                str.append(word2.charAt(i));
+            }
+            i++;
         }
-        return str;
+        return str.toString();
     }
 }

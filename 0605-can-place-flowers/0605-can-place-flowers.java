a8761251f36f0502debc66e5fd5ffb337c2571e9
Medhini K Shetty <119508392+Medhini-ks@users.noreply.github.com>
2024-08-08 12:26:01 +0530

    Time: 1 ms (97.56%), Space: 45.4 MB (64.99%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,20 +1,21 @@
 class Solution {
-    public boolean canPlaceFlowers(int[] a, int n)
-    {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int cnt=0;
-     for(int i=0;i<a.length;i++)
-     {
-         if(a[i]==0)
-         {
-             if((i==0 || a[i-1]==0 ) && (i==a.length-1 || a[i+1]==0))
-             {
-                 cnt++;
-                 a[i]=1;
-             }
-            
-         }
-        
-     }
-     return cnt>=n;  
+       for(int i=0;i<flowerbed.length;i++)
+       {
+        if(flowerbed[i]==0)
+        {
+            if((i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0))
+           {
+             cnt++;
+             flowerbed[i]=1;
+           }
+        }
+       }
+       
+               
+         
+       
+        return cnt>=n;
     }
 }

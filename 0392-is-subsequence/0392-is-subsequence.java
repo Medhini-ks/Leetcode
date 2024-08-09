3d6f0049b9550dfbc6d437d2f322054162b0b1fc
Medhini K Shetty <119508392+Medhini-ks@users.noreply.github.com>
2024-08-09 21:30:14 +0530

    Time: 1 ms (92.61%), Space: 41.5 MB (22.31%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) 
+    {
+        if(s.length()>t.length() )
+        return false;
+        if(s.length()==0)
+        return true;
+        int j=0;
+      for(int i=0;i<t.length();i++)
+      {
+        if(s.charAt(j)==t.charAt(i) && j<s.length())
+        {
+            j++;
+            if(j==s.length())
+            {
+                return true;
+            }
+        }
+      } 
+      return false; 
+    }
+}

4ba40fd4911deee2d4757e0b84966f103b390d87
Medhini K Shetty <119508392+Medhini-ks@users.noreply.github.com>
2024-08-12 10:41:47 +0530

    Time: 0 ms (100%), Space: 40.3 MB (58.12%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int kthFactor(int n, int k) 
-    {
-     int count=0;
-     int i=1;
-     while(i<=n)
-     {
-        if(n%i==0)
-        {
-            count++;
-            if(count==k)
-             { return i;
-             }
+    public int kthFactor(int n, int k) {
+        int count = 0;
+        int i = 1;
+        while (i <= n) {
+            if (n % i == 0) {
+                count++;
+                if (count == k) {
+                    return i;
+                }
+            }
+            i++;
         }
-        i++;
-     }  
-     return -1; 
+        return -1;
     }
 }
